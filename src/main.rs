a987c4040de0b843578449789d5bfce19f1ab30f
Antonin Hackenberg <antonin.hackenberg@gmail.com>
2020-10-19 20:42:50 +0200

    Add Iterator

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,12 @@
-extern crate termion;
-use termion::color;
 // Rules
 // 1. Any live cell with two or three live neighbours survives.
 // 2. Any dead cell with three live neighbours becomes a live cell.
-// 3. All other live cells die in the next generation. Similarly, 
+// 3. All other live cells die in the next generation. Similarly,
 //    all other dead cells stay dead.
-fn main() {
-  let live_cells = vec![
-    vec![0, 1],
-    vec![0, 2],
-    vec![0, 3],
-    vec![1, 2],
-    vec![4, 1],
-    vec![4, 2],
-    vec![4, 3],
-  ];
-  let rounds = 10;
-  let size = 10;
 
-  println!("{}Round 0", color::Fg(color::Yellow));
-  print_matrix((&live_cells).to_vec(), size);
-  let mut results = vec![live_cells];
+type LiveCells = Vec<Vec<i32>>;
 
-  for i in 1..=rounds {
-    println!("\nRound {}", i);
-
-    let last_item = &results[results.len() - 1];
-
-    let round = calculate_round((&last_item).to_vec(), size);
-    print_matrix((&round).to_vec(), size);
-    results.push(round)
-  }
-}
-
-fn calculate_live_neigbours(ir: i32, ii: i32, live_cells: Vec<Vec<i32>>) -> i32 {
+fn calculate_live_neigbours(ir: i32, ii: i32, live_cells: LiveCells) -> i32 {
   let calc = [
     [-1, -1],
     [-1, 0],
@@ -59,7 +32,7 @@ fn calculate_live_neigbours(ir: i32, ii: i32, live_cells: Vec<Vec<i32>>) -> i32 
   neigbours
 }
 
-fn print_matrix(live_cells: Vec<Vec<i32>>, size: i32) {
+fn print_matrix(live_cells: LiveCells, size: i32, round: i32) {
   let mut matrix = vec![];
   for ir1 in 0..size {
     let mut row = String::from("");
@@ -74,13 +47,15 @@ fn print_matrix(live_cells: Vec<Vec<i32>>, size: i32) {
     matrix.push(row)
   }
 
+  println!("Round {:?}", round);
+
   for row in matrix.iter() {
     println!("{:?}", row)
   }
 }
 
-fn calculate_round(live_cells: Vec<Vec<i32>>, size: i32) -> Vec<Vec<i32>> {
-  let mut matrix = vec![];
+fn calculate_round(live_cells: LiveCells, size: i32) -> LiveCells {
+  let mut next_live_cells = vec![];
   for ir in 0..size {
     for ii in 0..size {
       let el = vec![ir, ii];
@@ -88,13 +63,60 @@ fn calculate_round(live_cells: Vec<Vec<i32>>, size: i32) -> Vec<Vec<i32>> {
       let number_of_live_neibours = calculate_live_neigbours(ir, ii, (&live_cells).to_vec());
 
       match (live_cells.contains(&el), number_of_live_neibours) {
-        (false, 3) => matrix.push(el),
-        (true, 2) => matrix.push(el),
-        (true, 3) => matrix.push(el),
+        (false, 3) => next_live_cells.push(el),
+        (true, 2) => next_live_cells.push(el),
+        (true, 3) => next_live_cells.push(el),
         _ => (),
       }
     }
   }
 
-  matrix
+  next_live_cells
+}
+
+struct GameOfLife {
+  live_cells: LiveCells,
+  round: i32,
+  size: i32,
+}
+
+impl Iterator for GameOfLife {
+  type Item = (i32, LiveCells);
+  fn next(&mut self) -> Option<(i32, LiveCells)> {
+    let round = calculate_round((&self.live_cells).to_vec(), self.size);
+
+    self.live_cells = round;
+    self.round = self.round + 1;
+
+    Some((self.round, (&self.live_cells).to_vec()))
+  }
+}
+
+fn game_of_life(size: i32, live_cells: LiveCells) -> GameOfLife {
+  GameOfLife {
+    live_cells: live_cells,
+    round: 0,
+    size: size,
+  }
+}
+
+fn main() {
+  let live_cells = vec![
+    vec![0, 1],
+    vec![0, 2],
+    vec![0, 3],
+    vec![1, 2],
+    vec![4, 1],
+    vec![4, 2],
+    vec![4, 3],
+  ];
+
+  let size = 10;
+  let rounds = 20;
+
+  print_matrix((&live_cells).to_vec(), size, 0);
+
+  for i in game_of_life(size, live_cells).take(rounds) {
+    print_matrix(i.1, size, i.0);
+  }
 }
